f33ea740fe7e81011008f00f8d4d456aeb319cfa
DrageFabeldyr <vittu_perkele@list.ru>
2017-07-25 10:38:35 +0300

    корректная работа смещения игрока

--- game.cpp
+++ game.cpp
@@ -1,42 +1,20 @@
 #include "game.h"
 #include "ui_game.h"
 
-game::game(QWidget *parent) : QWidget(parent), ui(new Ui::game)
+#include <QDebug>
+
+game::game(QWidget *parent) : QWidget(parent)//, ui(new Ui::game)
 {
     setStyleSheet("background-color: MediumSpringGreen;"); // заливка окна - https://ru.wikipedia.org/wiki/Википедия:Таблица_цветов
-
-    QFont f("Arial", 10, QFont::Bold);
-
-    ball_x_speed = ball_speed;
-    ball_y_speed = -ball_speed;
+    f.setFamily("Arial");
+    f.setBold(true);
+    f.setPixelSize(13);
 
     // установка сцены
     scene = new QGraphicsScene(QRectF(0, 0, 800, 600));
     grview = new QGraphicsView(scene, this);
 
     scene->setBackgroundBrush(QBrush(Qt::black)); // покраска сцены
-
-    // добавляем "игрока"
-    player = new QGraphicsRectItem;
-    player = scene->addRect(0, 0, player_width, player_height, QPen(Qt::green), QBrush(Qt::green, Qt::Dense5Pattern)); // размеры, цвет рамки, цвет заливки
-    player->setPos((scene->width() - player_width) / 2, scene->height() - 2 * player_height);
-
-    create_level(scene); // заполняем сцену кубиками
-
-    // добавляем "мячик"
-    ball = new QGraphicsEllipseItem;
-    ball = scene->addEllipse(0, 0, ball_size, ball_size, QPen(Qt::red), QBrush(Qt::red));
-    ball->setPos((scene->width() - ball_size) / 2, scene->height() - 2 * player_height - ball_size);
-
-    // Создаем игровой таймер для перерисовки сцены
-    timer = new QTimer();
-    connect(timer, SIGNAL(timeout()), this, SLOT(onTimer()));
-    timer->setInterval(1000 / (100 + 20 * speed)); // скорость обновления экрана
-    timer->start();
-
-    // Создаём таймер для регулировки частоты нажатия кнопки паузы
-    pause_timeout_timer = new QTimer();
-    connect(pause_timeout_timer, SIGNAL(timeout()), this, SLOT(can_pause()));
 
     game_layout = new QVBoxLayout;   // игра
     stat_layout = new QVBoxLayout;   // настройки, параметры и очки
@@ -45,7 +23,6 @@ game::game(QWidget *parent) : QWidget(parent), ui(new Ui::game)
     game_layout->addWidget(grview);
     grview->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     grview->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
     hello_text = new QLabel;
     level_text = new QLabel;
     score_text = new QLabel;
@@ -72,67 +49,33 @@ game::game(QWidget *parent) : QWidget(parent), ui(new Ui::game)
     stat_layout->addWidget(bricks_text);
     stat_layout->addStretch(1); // чтобы сдвинуть всё наверх
 
+
     main_layout->addLayout(game_layout);
     main_layout->addLayout(stat_layout);
     setLayout(main_layout);
-//    grview->show();
+    main_layout->setSizeConstraint(QLayout::SetFixedSize); // запрет изменения размеров
+
+    // Создаем таймер для выхода из отрисовки сцены и запуска меню
+    create_timer = new QTimer();
+    connect(create_timer, SIGNAL(timeout()), this, SLOT(start_menu()));
+    create_timer->setInterval(100); // время запуска игры
+    create_timer->start();
+    this->setFocus(); // чтобы ловились стрелки
 }
 
 game::~game()
 {
-    delete ui;
+    //delete ui;
 }
 
 void game::onTimer()
 {
     // если уровень пройден
-    if (bricks.size() <= 30)
+    if (bricks.size() <= 10)
     {
         create_level(scene);
         level_value++;
     }
-
-    // обработка нажатия клавиш управления
-    if (GetAsyncKeyState(VK_LEFT))
-    {
-         player->moveBy(-player_speed, 0);
-    }
-    if (GetAsyncKeyState(VK_RIGHT))
-    {
-         player->moveBy(player_speed, 0);
-    }
-    if (GetAsyncKeyState(VK_ESCAPE))
-    {
-        if (!pause_timeout)
-        {
-            pause_timeout = true;
-            pause_timeout_timer->start(300); // допустимая частота нажатия клавиши паузы
-            if (ball_x_speed == 0)
-            {
-                ball_x_speed = ball_x_speed_old;
-                ball_y_speed = ball_y_speed_old;
-            }
-            else
-            {
-                ball_x_speed_old = ball_x_speed;
-                ball_y_speed_old = ball_y_speed;
-                ball_x_speed = 0;
-                ball_y_speed = 0;
-            }
-        }
-    }
-
-    // Проверка столкновения игрока с левой стенкой
-    if (player->x() < 0)
-    {
-        player->setX(1);
-    }
-    // Проверка столкновения игрока с правой стенкой
-    if (player->x() + player->rect().width() + player_speed > scene->width())
-    {
-        player->setX(scene->width() - player->rect().width() - 1);
-    }
-
 
     // обработка событий шарика
     ball->moveBy(ball_x_speed, ball_y_speed); // двигать шарик
@@ -210,6 +153,19 @@ void game::onTimer()
                 bricks.removeAt(i);
             }
         }
+
+        if (keyboard_state[2])
+        {
+            // Проверка столкновения игрока с левой стенкой
+            if (player->x() - player_speed > -1)
+                player->moveBy(-player_speed, 0);
+        }
+        if (keyboard_state[3])
+        {
+            // Проверка столкновения игрока с правой стенкой
+            if (player->x() + player->rect().width() + player_speed < scene->width() + 1)
+                player->moveBy(player_speed, 0);
+        }
     }
 
     // обновляем результаты
@@ -219,28 +175,52 @@ void game::onTimer()
     bricks_text->setText(QString("Bricks: %1").arg(bricks.size()));
 }
 
-// функция разрешения повторного нажатия кнопки паузы
-void game::can_pause()
-{
-    pause_timeout =  false;
-}
-
 // функция создания уровней
 void game::create_level(QGraphicsScene *level_scene)
 {
     int i, j = 0;
 
-    while ((50 * j + brick_width) <  scene->width())
+    while (((brick_width + brick_w_spacing) * j + brick_width) <  scene->width()) // пока можно всунуть ещё 1 кирпич по ширине
     {
         for (i = 0; i <= j; i++)
         {
             QGraphicsRectItem *brick = new QGraphicsRectItem;
             brick = level_scene->addRect(0, 0, brick_width, brick_height, QPen(Qt::cyan), QBrush(Qt::cyan)); // размеры, цвет рамки, цвет заливки
-            brick->setPos(50 * i, 20 * j);
+            brick->setPos((brick_width + brick_w_spacing) * i, (brick_height + brick_h_spacing) * j);
             bricks.append(brick);
         }
         j++;
     }
+}
+
+// запуск игры
+void game::start_game()
+{
+    gameplay = true;
+    ball_x_speed = /*0;/*/ball_speed;
+    ball_y_speed = /*0;/*/-ball_speed;
+
+    // добавляем "игрока"
+    player = new QGraphicsRectItem;
+    player = scene->addRect(0, 0, player_width, player_height, QPen(Qt::green), QBrush(Qt::green, Qt::Dense5Pattern)); // размеры, цвет рамки, цвет заливки
+    player->setPos((scene->width() - player_width) / 2, scene->height() - 2 * player_height);
+
+    create_level(scene); // заполняем сцену кубиками
+
+    // добавляем "мячик"
+    ball = new QGraphicsEllipseItem;
+    ball = scene->addEllipse(0, 0, ball_size, ball_size, QPen(Qt::red), QBrush(Qt::red));
+    ball->setPos((scene->width() - ball_size) / 2, scene->height() - 2 * player_height - ball_size);
+
+    // Создаем игровой таймер для перерисовки сцены
+    timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(onTimer()));
+    timer->setInterval(1000 / (100 + 20 * speed)); // скорость обновления экрана
+    timer->start();
+
+    // Создаём таймер для регулировки частоты нажатия кнопки паузы
+    keypress_timer = new QTimer();
+    connect(keypress_timer, SIGNAL(timeout()), this, SLOT(can_press_again()));
 }
 
 // обработка "game over"а
@@ -248,3 +228,118 @@ void game::end_game()
 {
     exit(0);
 }
+
+// функция разрешения повторного нажатия кнопки паузы
+void game::can_press_again()
+{
+    keypress_timeout =  false;
+}
+
+
+// работа с меню
+void game::start_menu()
+{
+    if ((keyboard_state[4] == true) && (!gameplay))
+        start_game();
+}
+
+bool game::event(QEvent *event)
+{
+    if (event->type() == QEvent::KeyPress)
+    {
+        QKeyEvent *keyEvent = (QKeyEvent *)event;
+
+        qDebug() << keyEvent->key() << "key"; // вывод кода клавиши в окно вывода
+        qDebug() << keyEvent->nativeScanCode() << "native"; // вывод кода клавиши в окно вывода
+
+        if (keyEvent->key() == 0x01000013) // up
+            keyboard_state[0] = true;
+        if (keyEvent->key() == 0x01000015) // down
+            keyboard_state[1] = true;
+        if (keyEvent->key() == 0x01000012) // left
+            keyboard_state[2] = true;
+        if (keyEvent->key() == 0x01000014) // right
+            keyboard_state[3] = true;
+        if ((keyEvent->key() == 0x01000004) || (keyEvent->key() == 0x01000005)) // enter || num-enter
+            keyboard_state[4] = true;
+        if (keyEvent->key() == 0x01000008) // pause
+            keyboard_state[5] = true;
+        qDebug() << "key pressed";
+    }
+    if (event->type() == QEvent::KeyRelease)
+    {
+        QKeyEvent *keyEvent = (QKeyEvent *)event;
+
+        if (keyEvent->key() == 0x01000013) // up
+            keyboard_state[0] = false;
+        if (keyEvent->key() == 0x01000015) // down
+            keyboard_state[1] = false;
+        if (keyEvent->key() == 0x01000012) // left
+            keyboard_state[2] = false;
+        if (keyEvent->key() == 0x01000014) // right
+            keyboard_state[3] = false;
+        if ((keyEvent->key() == 0x01000004) || (keyEvent->key() == 0x01000005)) // enter || num-enter
+            keyboard_state[4] = false;
+        if (keyEvent->key() == 0x01000008) // pause
+            keyboard_state[5] = false;
+
+        qDebug() << "key released";
+    }
+
+
+    return QWidget::event(event); // т.к. стандартная функция bool, надо что-то возвращать
+}
+
+/*
+void game::keyPressEvent(QKeyEvent *event)
+{
+    int key = event->key(); // event->key() - целочисленный код клавиши
+    qDebug() << event->key() << "key"; // вывод кода клавиши в окно вывода
+    qDebug() << event->nativeScanCode() << "native"; // вывод кода клавиши в окно вывода
+
+    if (gameplay)
+    {
+        /*
+        // обработка нажатия клавиш управления
+        if (key == Qt::Key_Left)
+        {
+            // Проверка столкновения игрока с левой стенкой
+            if (player->x() - player_speed > -1)
+                player->moveBy(-player_speed, 0);
+        }
+        if (key == Qt::Key_Right)
+        {
+            // Проверка столкновения игрока с правой стенкой
+            if (player->x() + player->rect().width() + player_speed < scene->width() + 1)
+                player->moveBy(player_speed, 0);
+        }
+        */
+
+/*
+        if (key == Qt::Key_Pause && !keypress_timeout)
+        {
+            if (ball_x_speed == 0)
+            {
+                ball_x_speed = ball_x_speed_old;
+                ball_y_speed = ball_y_speed_old;
+            }
+            else
+            {
+                ball_x_speed_old = ball_x_speed;
+                ball_y_speed_old = ball_y_speed;
+                ball_x_speed = 0;
+                ball_y_speed = 0;
+            }
+            keypress_timeout = true;
+        }
+    }
+    else
+    {
+        if (key == Qt::Key_Return)
+            start_game();
+    }
+
+    if (keypress_timeout)
+        keypress_timer->start(500); // допустимая частота нажатия клавиши паузы
+}
+*/
--- game.h
+++ game.h
@@ -9,7 +9,8 @@
 #include <windows.h>
 #include <QHBoxLayout>
 #include <QLabel>
-#include<qmath.h>
+#include <qmath.h>
+#include <QKeyEvent>
 
 namespace Ui {
 class game;
@@ -26,13 +27,14 @@ public:
 
 private:
     QGraphicsView *grview;
-    QTimer *timer;          // игровой таймер перерисовки
-    QTimer *pause_timeout_timer; // таймер задержки повторной обработки нажатия паузы
-    Ui::game *ui;
+    QTimer *timer;                         // игровой таймер перерисовки
+    QTimer *keypress_timer;  // таймер задержки повторной обработки нажатия паузы
+    QTimer *create_timer;                // таймер для выхода из процедуры создания формы и создания меню
+    //Ui::game *ui;
 
     QGraphicsRectItem *player;  // игрок
     QGraphicsEllipseItem *ball; // мячик
-    QList<QGraphicsRectItem*> bricks;
+    QList<QGraphicsRectItem*> bricks; // кирпичики
 
     // элементы отображения
     QVBoxLayout *game_layout;   // игра
@@ -43,33 +45,51 @@ private:
     QLabel *score_text;         // текст очков
     QLabel *speed_text;        // текст скорости шарика
     QLabel *bricks_text;        // текст оставшихся кирпичиков
+    QFont f;
 
     // переменные
-    int player_height = 10;     // толщина игрока
-    int player_width = 200;    // ширина игрока
-    int ball_size = 12;           // диаметр шарика
-    int brick_height = 10;      // толщина кирпичей
-    int brick_width = 40;       // ширина кирпичей
+    int player_height = 10;       // толщина игрока
+    int player_width = 200;      // ширина игрока
+    int ball_size = 12;             // диаметр шарика
+    int brick_height = 10;        // высота кирпичей
+    int brick_h_spacing = 10;  // промежуток между кирпичами по вертикали
+    int brick_width = 40;         // ширина кирпичей
+    int brick_w_spacing = 10; // промежуток между кирпичами по горизонтали
 
     int speed = 1;                // переменная скорости обновления экрана
     int ball_speed = 2;         // смещение шарика (скорость)
     int ball_x_speed;           // горизонтальная скорость шарика
     int ball_y_speed;           // вертикальная скорость шарика
-    int ball_x_speed_old;     // для сохранения горизонтальной скорости шарика
-    int ball_y_speed_old;     // для сохранения вертикальной скорости шарика
-    int player_speed = 10;   // скорость перемещения игрока
+    int ball_x_speed_old = 0;     // для сохранения горизонтальной скорости шарика
+    int ball_y_speed_old = 0;     // для сохранения вертикальной скорости шарика
+    double player_speed = 0.1;   // скорость перемещения игрока
 
     int score_value = 0;       // количество заработанных очков
     int level_value = 1;        // номер уровня
 
+    bool keyboard_state[6] = {0}; // массив состояний клавиш
+    /* список клавиш управления (всего: 6):
+     * 0 / 1 - вверх / вниз - для меню
+     * 2 / 3 - влево / вправо - для игры
+     * 4 - ввод - для переходов по меню
+     * 5 - пауза
+     */
+
     // флаги
-    bool pause_timeout = false;
+    bool keypress_timeout = false;
+    bool gameplay = false;  // 0 - меню, 1 - игра
 
 private slots:
     void onTimer(); // функция обновления экрана
-    void can_pause(); // функция разрешения повторного нажатия кнопки паузы
+    void can_press_again(); // разрешения повторного нажатия кнопки
     void create_level(QGraphicsScene *level_scene); // функция создания уровня
+    void start_menu(void); // функция вывода меню
+    void start_game(void);// функция выхода из игры
     void end_game(void);// функция выхода из игры
+
+protected:
+    virtual bool event(QEvent *event); // обработчик событий
+   //virtual void keyPressEvent(QKeyEvent *event);
 };
 
 #endif // GAME_H
